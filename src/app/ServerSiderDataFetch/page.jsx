863a6a24288a309593d2aaf4bb5d828e78d01d93
Zohaib Arshad <zohaibfta@gmail.com>
2025-02-10 19:35:04 +0500

    Server and client side both together

--- src/app/ServerSiderDataFetch/page.jsx
+++ src/app/ServerSiderDataFetch/page.jsx
@@ -19,7 +19,10 @@ const ServerSideDataFetch = async() => {
           <div key={user.id} className='bg-gray-700 mb-3 p-3'>
             <h1 className='text-blue-300 font-bold'>{user.id} {user.name}</h1>
             <h1>{user.body}</h1>
-            <Btn data = {user.body}/> // if we want to add button onclick effect, we need make it server side but this is server side data fetch, so we make any file btn and add her refence
+
+            {/* <Btn data = {user.body}/> // if we want to add button onclick effect, we need make it server side but this is server side data fetch, so we make any file btn and add her refence
+            // we can use both server side and client side like this */}
+            
           </div>
         ))
       }
